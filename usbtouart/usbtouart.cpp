08d8273ae87e99ae314e1290082cb4d62a7946d6
steven miller <sjmiller0403@gmail.com>
2024-09-10 23:27:54 -0400

    modified strings that are output over uart

--- usbtouart/usbtouart.cpp
+++ usbtouart/usbtouart.cpp
@@ -1,13 +1,14 @@
 //usbtouart.cpp
-/* Author(s): Steven Miller, Kieran
+/* Author(s): Steven, Kieran
 Date created: September 5 2024
 Date updated: September 10 2024
 Purpose: Allows inputs from USB controller(s) and sends out over UART
-Notes: USB devices are accessed in Linux using file descriptors, which are just files themselves.
+Notes: USB devices are accessed in Linux using file descriptors, which are just files themselves. Assisted by Claude.ai to help understand and organize code
 log:
-    $ September 5 2024, Steven Miller: Initial creation
+    $ September 5 2024, Steven: Initial creation
     $ September 9 2024, Kieran: Update to enable Axes Reading.
     $ September 10 2024, Kieran: Enabled UART on Raspberry Pi. Able to read controller buttons. 
+    $ September 10 2024, Steven: changed UART descriptor file, translated button presses into proper bit strings
  */
 
 #include <iostream>
@@ -27,14 +28,25 @@ log:
 
 // Directory of file descriptor
 #define USB_DEVICE_DIRECTORY "/dev/input/js0"
-#define UART_DEVICE "/dev/serial0" 
+#define UART_DEVICE "/dev/ttyAMA0" 
+#define BAUDRATE B115200
+#define A_BITS      0b00000001
+#define B_BITS      0b00000010
+#define SELECT_BITS 0b00000100
+#define START_BITS  0b00001000
+#define UP_BITS     0b00010000
+#define DOWN_BITS   0b00100000
+#define LEFT_BITS   0b01000000
+#define RIGHT_BITS  0b10000000
 
 // Joystick event struct
 struct js_event usb_device_event;
 
-int setup_uart(const char* device) {
-    int uart_fd = open(device, O_RDWR | O_NOCTTY | O_NDELAY);
-    if (uart_fd < 0) {
+int setup_uart(const char* device) 
+{
+    int8_t uart_fd = open(device, O_RDWR | O_NOCTTY | O_NDELAY);
+    if (uart_fd < 0) 
+    {
         std::cerr << "Error opening UART device: " << strerror(errno) << std::endl;
         return -1;
     }
@@ -43,8 +55,8 @@ int setup_uart(const char* device) {
     tcgetattr(uart_fd, &options);
 
     // Set baud rate
-    cfsetispeed(&options, B115200);
-    cfsetospeed(&options, B115200);
+    cfsetispeed(&options, BAUDRATE);
+    cfsetospeed(&options, BAUDRATE);
 
     // 8N1 mode
     options.c_cflag &= ~PARENB;
@@ -69,7 +81,8 @@ int setup_uart(const char* device) {
     options.c_cc[VMIN]  = 0;
     options.c_cc[VTIME] = 0;
 
-    if (tcsetattr(uart_fd, TCSANOW, &options) < 0) {
+    if (tcsetattr(uart_fd, TCSANOW, &options) < 0) 
+    {
         std::cerr << "Error setting UART attributes: " << strerror(errno) << std::endl;
         close(uart_fd);
         return -1;
@@ -103,7 +116,7 @@ int main()
     std::cout << "Number of axes: " << (int)usb_device_numberofaxes << std::endl;
 
     // Setup UART
-    int uart_fd = setup_uart(UART_DEVICE);
+    int8_t uart_fd = setup_uart(UART_DEVICE);
     if (uart_fd < 0) {
         close(usb_device);
         return -1;
@@ -112,7 +125,7 @@ int main()
 
     fd_set readfds;
     struct timeval tv;
-    time_t last_check = 0;
+   // time_t last_check = 0;
 
     while(1) {
         FD_ZERO(&readfds);
@@ -121,13 +134,92 @@ int main()
         tv.tv_sec = 0;
         tv.tv_usec = 100000; // 100ms timeout
 
-        int ret = select(usb_device + 1, &readfds, NULL, NULL, &tv);
+        int16_t ret = select(usb_device + 1, &readfds, NULL, NULL, &tv);
 
-        if (ret > 0) {
-            if (read(usb_device, &usb_device_event, sizeof(usb_device_event)) == sizeof(usb_device_event)) {
+        if (ret > 0) 
+        {
+            if (read(usb_device, &usb_device_event, sizeof(usb_device_event)) == sizeof(usb_device_event)) 
+            {
                 // Process the event
-                char buffer[32];
-                int len;
+                //char buffer[32];
+                //int16_t len;
+                //NES buttons and how they show up in linux
+                /*
+                 *A: button 1
+                 *B: button 2
+                 *Select: button 8
+                 *Start: button 9
+                 *Up: axis 1, pressed = -32767, released = 0
+                 *Down:axis 1, pressed = 32767, released = 0
+                 *Left: axis 0, pressed = -32767, released = 0
+                 *Right: axis 0, pressed = 32767, released = 0  
+                 */
+                //craft string to send over UART
+                uint8_t controller_byte = 0;
+                if(usb_device_event.type == JS_EVENT_BUTTON)
+                {
+                    //A button pressed
+                    if((uint8_t)usb_device_event.number == 1 && (uint8_t)usb_device_event.value == 1)
+                    {
+                        controller_byte = controller_byte | A_BITS;
+                    }
+                    //B button pressed
+                    if((uint8_t)usb_device_event.number == 2 && (uint8_t)usb_device_event.value == 1)
+                    {
+                         controller_byte = controller_byte | B_BITS;
+                    }
+                    //select button pressed
+                    if((uint8_t)usb_device_event.number == 8 && (uint8_t)usb_device_event.value == 1)
+                    {
+                         controller_byte = controller_byte | SELECT_BITS;
+                    }
+                    //start button pressed
+                    if((uint8_t)usb_device_event.number == 9 && (uint8_t)usb_device_event.value == 1)
+                    {
+                         controller_byte = controller_byte | START_BITS;
+                    }
+                }
+                if(usb_device_event.type == JS_EVENT_AXIS)
+                {
+                    //up or down button pressed
+                    if((uint8_t)usb_device_event.number == 1)
+                    {
+                        //up button pressed
+                        if((int16_t)usb_device_event.value == -32767)
+                        {
+                              controller_byte = controller_byte | UP_BITS;
+                        }
+                        //down button pressed
+                        if((int16_t)usb_device_event.value == 32767)
+                        {
+                             controller_byte = controller_byte | DOWN_BITS;
+                        }
+                    }
+                    //left or right button pressed
+                    if((uint8_t)usb_device_event.number == 0)
+                    {
+                        //left button pressed
+                        if((int16_t)usb_device_event.value == -32767)
+                        {
+                              controller_byte = controller_byte | LEFT_BITS;
+                        }
+                        //right button pressed
+                        if((int16_t)usb_device_event.value == 32767)
+                        {
+                             controller_byte = controller_byte | RIGHT_BITS;
+                        }
+                    }
+                }
+                //len = snprintf(controller_byte, sizeof(controller_byte), controller_byte);
+                if (write(uart_fd, &controller_byte, sizeof(controller_byte)) != sizeof(controller_byte)) 
+                {
+                            std::cerr << "UART write error: " << strerror(errno) << std::endl;
+                } 
+                else 
+                {
+                            std::cout << "UART sent: " << std::to_string(controller_byte) << std::endl;
+                }
+                /*
                 switch (usb_device_event.type) {
                     case JS_EVENT_BUTTON:
                         std::cout << "Button " << (int)usb_device_event.number << " " 
@@ -152,26 +244,37 @@ int main()
                         }
                         break;
                 }
+                */
             }
-        } else if (ret == 0) {
+        } 
+        else if (ret == 0) 
+        {
             // Timeout, could do other processing here
-        } else {
+        } 
+        else 
+        {
             std::cerr << "select() error: " << strerror(errno) << std::endl;
             break;
         }
 
+        /*
         // Check gamepad name periodically
         time_t now = time(NULL);
-        if (now - last_check > 5) { // Check every 5 seconds
+        if (now - last_check > 5) 
+        { // Check every 5 seconds
             char name[128];
-            if (ioctl(usb_device, JSIOCGNAME(sizeof(name)), name) >= 0) {
+            if (ioctl(usb_device, JSIOCGNAME(sizeof(name)), name) >= 0) 
+            {
                 std::cout << "Gamepad: " << name << std::endl;
-            } else {
+            } 
+            else 
+            {
                 std::cout << "Gamepad unplugged. Exiting...\n";
                 break;
             }
             last_check = now;
         }
+        */
     }
 
     // Close file descriptors
